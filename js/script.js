f94350e77a2c95ed76e6fbbb9547a4390b148d8c
Matt Yost <matt.yost2@gmail.com>
2016-07-20 00:36:47 -0500

    Finished Tshirst Color Selector
    Fixed all issues for showing the corresponding elements for Design
    choice

--- js/script.js
+++ js/script.js
@@ -1,27 +1,42 @@
 $("#name").focus();
-$("#color").children().hide();
+$("#colors-js-puns").hide();
 
-$("#title").on("change",function(){
-  if($(this).val() === "other"){
-    $(".basic-info").append("<input type='text' id='other-title' placeholder='Your Title'>");
-    $("#other-title").focus();
-  } else {
-    $('#other-title').remove()
-  }
-});
+var hideChildren = function(e){
+  $(e).children().hide();
+}
 
-$("#design").on("change", function(){
-  $("#color").children().hide();
-  if($(this).val() === "js puns"){
-    $("option[value='cornflowerblue']").show();
-    $("option[value='darkslategrey']").show();
-    $("option[value='gold']").show();
-    $("#color").first().attr("selected", "selected");
-  } else if($(this).val() === "heart js") {
-    $("option[value='tomato']").show();
-    $("option[value='steelblue']").show();
-    $("option[value='dimgrey']").show();
-  } else {
-    $("option[value='default']").show();
-  }
-});
+var customTitle = function(){
+  $("#title").on("change",function(){
+    if($(this).val() === "other"){
+      $(".basic-info").append("<input type='text' id='other-title' placeholder='Your Title'>");
+      $("#other-title").focus();
+    } else {
+      $('#other-title').remove()
+    }
+  });
+}
+
+var tshirtColor = function(){
+  $("#design").on("change", function(){
+    $("#colors-js-puns").show();
+    if($(this).val() === "js puns"){
+      hideChildren("#color");
+      $("option[value='cornflowerblue']").show();
+      $("option[value='darkslategrey']").show();
+      $("option[value='gold']").show();
+      $("#color").val('cornflowerblue').show();
+    } else if($(this).val() === "heart js") {
+      hideChildren("#color");
+      $("option[value='tomato']").show();
+      $("option[value='steelblue']").show();
+      $("option[value='dimgrey']").show();
+      $("#color").val('tomato').show();
+    } else {
+      hideChildren("#color");
+      $("#color").val('default');
+    }
+  });
+}
+
+customTitle();
+tshirtColor();
