e650600a4f15771c8950129d3e841cc2e80e08c3
Matt Yost <matt.yost2@gmail.com>
2016-08-23 15:25:11 -0500

    Lots of things
    Really bad inexplicit commit with lots of code and no comments

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 // On load focus on first field
 $("#name").focus();
+$("#zip-error").hide();
+$("#credit-error").hide();
 
 // Hides children of a given element
 var hideChildren = function(e){
@@ -63,30 +65,95 @@ var eventSelector = function(input, conflict){
 
 var totalCost = function(){
   $('.activities input').on('click', function() {
-      var total = 0;
-      $('.activities input:checked').each(function() {
-          total += parseInt($(this).val());;
-      });
-      $('#total-cost').text("Total cost: $" + total);
+    var total = 0;
+    $('.activities input:checked').each(function() {
+      total += parseInt($(this).val());
     });
-  }
+    $('#total-cost').text("Total cost: $" + total);
+  });
+}
 
-  var creditCard = function(){
-    //$("#payment").val("credit card");
+var payMethod = function(){
+  $("#paypal, #bitcoin").hide();
+  $("#payment").on("change", function() {
+    if($("#payment").val() === "credit card"){
+      $("#credit-card").show();
+      $("#paypal, #bitcoin").hide();
+    } else if($("#payment").val() === "paypal"){
+      $("#credit-card, #bitcoin").hide();
+      $("#paypal").show();
+      //ccIsValid = true;
+    } else if($("#payment").val() === "bitcoin") {
+      $("#credit-card, #paypal").hide();
+      $("#bitcoin").show();
+      //ccIsValid = true;
+    }
+  });
+}
 
-    $("#payment").on("change", function() {
-      if($("#payment").val() === "credit card"){
-        $("#paypal").hide();
-        $("#bitcoin").hide();
-      } else if($("#payment").val() === "paypal"){
-        $("#bitcoin").hide();
-        $("#paypal").show();
-      } else if($("#payment").val() === "bitcoin") {
-        $("#paypal").hide();
-        $("#bitcoin").show();
-      }
-    });
+var zipIsValid = false;
+var ccIsValid = false;
+var ccvIsValid = false;
+var nameIsValid = false;
+var emailIsValid = false;
+var activityIsValid = false;
+
+ccIsValid = $("#cc-num").validateCreditCard().valid;
+
+$("#credit-card").focusout(function(){
+  ccIsValid = $("#cc-num").validateCreditCard().valid;
+  if(ccIsValid){
+    $("label[for=cc-num]").css("color", "black");
+  } else {
+    $("label[for=cc-num]").css("color", "red");
   }
+});
+
+$("#zip").focusout(function(){
+  var zipLength = $("#zip").val().length;
+  if(zipLength < 5){
+    $("label[for=zip]").css("color", "red");
+  } else if(zipLength >= 5){
+    $("label[for=zip]").css("color", "black");
+    zipIsValid = true;
+  }
+  return zipIsValid;
+});
+
+$("#name").focusout(function(){
+  if($("#name").val() == ""){
+    $("label[for=name]").css("color", "red");
+    nameIsValid = false;
+  } else {
+    $("label[for=name]").css("color", "black");
+    nameIsValid = true;
+  }
+});
+
+$("#mail").focusout(function(){
+  var email = $("#mail").val();
+  if( /(.+)@(.+){2,}\.(.+){2,}/.test(email) ){
+    // valid email
+    $("label[for=mail]").css("color", "black");
+    emailIsValid = true;
+  } else {
+    // invalid email
+    $("label[for=mail]").css("color", "red");
+    emailIsValid = false;
+  }
+});
+
+var activitiesSelected = $(".activities input");
+$("button[type='submit']").click(function(){
+  activitiesSelected.each(function(){
+    if($(this).prop("checked")){
+      activityIsValid = true;
+    };
+  });
+  console.log(activityIsValid);
+});
+
+
 
 //Function calls
 customTitle();
@@ -96,4 +163,4 @@ eventSelector(libs,node);
 eventSelector(express,frameworks);
 eventSelector(node,libs);
 totalCost();
-creditCard();
+payMethod();
